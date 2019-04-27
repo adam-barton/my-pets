3d3705f7834cca8246e54bf2623e8a18e8244c60
github username <github email address>
2019-04-26 20:38:07 -0700

    Adds the framework for a React front-end

---
+++ my-pets/src/containers/PetsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PetsContainer extends Component {
+    render() {
+    return(
+        <h1>Hello from PetsContainer</h1>
+    )}
+
+}
+
+export default PetsContainer;

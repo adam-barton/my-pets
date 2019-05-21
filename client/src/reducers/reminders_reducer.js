6325228614cb00ca5b73eab93f6c39c86334e3f6
github username <adam.barton333@gmail.com>
2019-05-20 22:52:53 -0700

    Updated the RemindersReducer to update the state.today_reminders array after a reminder is deleted.

--- client/src/reducers/reminders_reducer.js
+++ client/src/reducers/reminders_reducer.js
@@ -3,14 +3,19 @@ export default function remindersReducer(state = {
     reminders: [],
     today_reminders: []
 }, action) {
+
+    let todaysReminders;
+    let reminderDate;
+    let today;
+
     switch (action.type) {
         case 'LOADING_REMINDERS':
         return {...state, loading: true }
 
         case 'FETCH_REMINDERS':
-            const todaysReminders = action.payload.filter(reminder => {
-                const reminderDate = new Date(reminder.displayable_date);
-                const today = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate())
+             todaysReminders = action.payload.filter(reminder => {
+                reminderDate = new Date(reminder.displayable_date);
+                today = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate())
                 if (reminderDate.toString() === today.toString()) {
                         return reminder
                 }
@@ -30,7 +35,16 @@ export default function remindersReducer(state = {
             }
         case 'DELETE_REMINDER':
             const reminders = state.reminders.filter(reminder => reminder.id !== action.payload.id)
-            return {...state, reminders }
+            todaysReminders = reminders.filter(reminder => {
+                    reminderDate = new Date(reminder.displayable_date);
+                    today = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate())
+                    if (reminderDate.toString() === today.toString()) {
+                            return reminder
+                    }
+                })
+            return {...state, 
+                reminders, 
+                today_reminders: todaysReminders}
 
         default: return state
     }
